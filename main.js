7f34b3321d51368b37b737887bec958302ebe726
Ricky Reyes <luisereyes52@yahoo.com>
2021-09-27 11:33:46 -0700

    first li working

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+
 const li_1 = document.querySelector('.li-1');
 const arrow_1 = document.querySelector('.arrow-1');
+const question_1 = document.querySelector('.q-1')
+const answer_1 = document.querySelector('.answer-1');
+var clicked = false;
+
 li_1.addEventListener('click', onClick);
 function onClick(e) {
-    arrow_1.style.transform = 'rotate(180deg)';
+    clicked = !clicked;
+    if (clicked) {
+        arrow_1.style.transform = 'rotate(180deg)';
+        answer_1.style.display = 'block';
+        question_1.style.color = 'black';
+        question_1.style.fontWeight = '700';
+    } else {
+        arrow_1.style.transform = 'rotate(0deg)';
+        answer_1.style.display = 'none';
+        question_1.style.color = 'var(--dark-grayish-blue)';
+        question_1.style.fontWeight = '400';
+    }
 }
-
-
